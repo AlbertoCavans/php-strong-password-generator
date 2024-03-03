2b011305f99f0948a026b2ac66544f0f347cf2b2
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-03 20:43:58 +0100

    Feat index.php - basic structure template

--- index.php
+++ index.php
@@ -13,8 +13,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container my-4">
+
+    <div class="my-3 d-flex justify-content-center align-items-center">
+        <h1>
+            Password generator
+        </h1>
+    </div>
+        <form class="row">
+            <div class="col-4">
+                <label for="password-parameter" class="form-label">
+                    Numero caratteri
+                </label>
+                <input type="text" class="form-control" name="password-parameter" id="password-parameter">
+            </div> 
+            <div class="col-1 d-flex">
+            <button class="btn btn-primary align-self-end">
+                Save 
+            </button>
+            </div>
+        </form>
+
+    </div>
     
 </body>
 </html>
