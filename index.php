543f81d751c460114a1367745c0aaf0323c0800c
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-04 16:17:35 +0100

    Feat index.php - password with mixed chars

--- index.php
+++ index.php
@@ -5,9 +5,14 @@ function create_password($password_length) {
     $new_password = "";
     for ($i = 0; $i < $password_length; $i++) {
     $letters = implode("", range("a", "z"));
-    $letter_index = rand(0, strlen($letters) - 1);
-    $random_letter = $letters[$letter_index];
-    $new_password .= $random_letter;
+    $LETTERS = implode("", range("A", "Z"));
+    $numbers = implode("", range("0", "9"));
+    $simbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    $mix_chars = $letters . $LETTERS . $numbers . $simbols;
+
+    $mix_chars_index = rand(0, strlen($mix_chars) - 1);
+    $random_mix_chars = $mix_chars[$mix_chars_index];
+    $new_password .= $random_mix_chars;
     }
 
     return $new_password;
